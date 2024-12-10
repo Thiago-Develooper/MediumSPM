5910763d0448e352004b4c0662208ec19330e3e3
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2024-12-10 09:26:31 -0300

    Initial Commit

---
+++ MediumSPM/MediumSPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumSPMApp.swift
+//  MediumSPM
+//
+//  Created by Thiago on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MediumSPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
